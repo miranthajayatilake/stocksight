8bec6457e6cb9592a5be51b815a26f3805b4a4bc
miranthajayatilake <mj.jayathilaka@gmail.com>
2021-11-16 09:22:29 +0530

    config

---
+++ config.py
@@ -0,0 +1,12 @@
+elasticsearch_host = "elasticsearch"
+elasticsearch_port = 9200
+elasticsearch_user = ""
+elasticsearch_password = ""
+consumer_key = "8C3BggZJ7rxnBbY8qKfgz2iMD"
+consumer_secret = "Oj354WMqdmfsBnm4dVKowqk44VpKnKbWKp4JfjKRAMHy0cyIzI"
+access_token = "350106545-ydBjsFq6aGh1wERReaD63A3G2HdxLlTHt6piYGVY"
+access_token_secret = "oidutldgTxarcwSP0vWT9qKhtFhEctBJDQAYC4PVDr18o"
+nltk_tokens_required = ("tesla")
+nltk_min_tokens = 1
+nltk_tokens_ignored = ("win", "giveaway")
+twitter_feeds = ["@elonmusk"]
